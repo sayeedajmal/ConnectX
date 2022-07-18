9ccb8ca8f3f497f51d8b64647ebf89f29d37eee0
Sayeed Ajmal <sayeedajmala06@gmail.com>
2022-07-18 18:45:11 +0530

    checking up Signup in SplashActivity

--- app/src/main/java/com/Strong/personalchat/Activity/SplashActivity.java
+++ app/src/main/java/com/Strong/personalchat/Activity/SplashActivity.java
@@ -10,21 +10,24 @@ import android.os.Handler;
 import com.Strong.personalchat.R;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.Objects;
+
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
     FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-            new Handler().postDelayed(() -> {
-                auth=FirebaseAuth.getInstance();
-                if (auth.getCurrentUser()!=null){
-                    startActivity(new Intent(SplashActivity.this, recentActivity.class));
-                }else{
-                    startActivity(new Intent(SplashActivity.this, purposeActivity.class));
-                }
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-            }, 180);
+        auth = FirebaseAuth.getInstance();
+        new Handler().postDelayed(() -> {
+            if (!Objects.requireNonNull(auth.getCurrentUser()).getUid().equals(auth.getUid())) {
+                startActivity(new Intent(SplashActivity.this, purposeActivity.class));
+            } else {
+                startActivity(new Intent(SplashActivity.this, recentActivity.class));
+            }
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }, 180);
     }
 }
